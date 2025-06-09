b614430781c72eb08b24c30e0ecac5858cb06d99
Sujal Patel <sujal7455@gmail.com>
2025-06-09 23:08:38 +0530

    index.css changes, and added some assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 function App() {
-
   return (
-    <>
-      <h1 className='text-3xl underline'>Hello World</h1>
-    </>
-  )
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+        <p>Search</p>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
