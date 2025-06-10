6253cd1c8d8d9d0c31b8e92a09ce9b71d8589d34
Sujal Patel <sujal7455@gmail.com>
2025-06-10 23:07:04 +0530

    fetching from tmdb api key added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Search from "./components/Search";
 
+const API_BASE_URL='https://api.themoviedb.org/3'
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
+
 function App() {
-  const [searchTerm, setSearchTerm] = useState(""); 
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState('')
+  const fetchMovies = async () => {
+    try {
+      
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage('Error fetching movies. Please try again later.')
+      
+    }
+  }
+  
+  useEffect(() => {
+
+  }, [])
   return (
     <main>
       <div className="pattern" />
