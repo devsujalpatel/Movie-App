cde020a2f1813d21e17971e93febcdc96ce8d9e8
Sujal Patel <sujal7455@gmail.com>
2025-06-10 22:02:45 +0530

    few changes

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,6 +1,20 @@
 
-const Search = ({searchTerm, setSearchTerm}) => {
-  return <div className="text-white text-3xl">{searchTerm}</div>;
+interface SearchProps {
+    searchTerm: string;
+    setSearchTerm: (searchTerm: string) => void;
+}
+
+const Search = ({searchTerm, setSearchTerm}: SearchProps) => {
+  return <div className="search">
+    <div>
+
+        <img src="search.svg" alt="search" />
+        <input type="text" placeholder="Search for thousands of movies" 
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        />
+    </div>
+  </div>
 };
 
 export default Search;
